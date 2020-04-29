c7a774e29d85aee5def3ab468dddf94f29859657
Pranshu Maheshwari <maheshwari.pranshu511@gmail.com>
2020-04-29 19:17:52 +0530

    Initialize the source

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <GL/gl.h>
+#include <GL/glu.h>
+#include <GL/glut.h>
+#include <iostream>
+#include <stdio.h>
+#include <math.h>
+
+using namespace std;
+
+int main() {
+
+    return 0;
+}
